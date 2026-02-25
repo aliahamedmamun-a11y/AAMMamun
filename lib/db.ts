226ecb09a318eb73f67c9a7bf0b34d3267e23cea
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-25 22:59:01 +0600

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+if (!MONGODB_URI) {
+  throw new Error("⚠️ MongoDB URI not found in .env file");
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "uchoice",
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  (global as any).mongoose = cached;
+  return cached.conn;
+}
