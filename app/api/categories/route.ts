b85e0afc5abc73fb7fe3a8d98600caedbd0e82d3
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-25 20:50:53 +0600

    Create route.ts

---
+++ app/api/categories/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/db";
+import Category from "@/models/Category";
+
+export async function GET() {
+  await dbConnect();
+  const categories = await Category.find({});
+  return NextResponse.json(categories);
+}
